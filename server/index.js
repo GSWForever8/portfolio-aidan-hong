dde10894e5c45b671101b57032df45286587a39d
GSWForever8 <119242962+GSWForever8@users.noreply.github.com>
2024-04-07 12:13:55 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(express.static(buildPath));
 
 
 const corsOptions={
-  origin:'https://portfolio-aidan-hong-frontend.onrender.com/contact',
+  origin:'https://portfolio-aidan-hong-frontend.onrender.com',
   optionsSuccessStatus:200
 }
 app.use(cors(corsOptions))
