5a70e5933f4d20c6521e868d4f866ce410083597
GSWForever8 <aidanhong8@icloud.com>
2024-06-04 12:22:17 -0700

    Styling update

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,52 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import logo from './pages/logo.png';
 import { Link, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import Contact from "./pages/Contact"
-import About from "./pages/About"
-import Projects from "./pages/Projects"
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
 import './App.css';
+
 function App() {
   return (
     <>
-        <div class="nav">
-          <ul>
-            <li class="bar"><Link to="/">Home</Link></li>
-            <li class="bar"><Link to="/about">About Me</Link></li>
-            <li class="bar"><Link to="/projects">Projects</Link></li>
-            <li class="bar"><Link to="/contact">Contact Me</Link></li>
-          </ul>
+      <nav className="navbar navbar-expand-lg nav">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            <img src={logo} alt="Logo" className="d-inline-block align-text-top App-logo" />
+          </Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link bar" to="/about">About Me</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link bar" to="/projects">Projects</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link bar" to="/contact">Contact Me</Link>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div class="container">
-          <div class="contentwrap">
-            <Routes>
-            <Route path="/" element={<Home />}/>
+      </nav>
+      <div className="container">
+        <div className="contentwrap">
+          <Routes>
+            <Route path="/" element={<Home />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/about" element={<About />} />
             <Route path="/projects" element={<Projects />} />
-            </Routes>
-          </div>
-          <footer class="footer">
-            <p>Aidan Hong 2024</p>
-        </footer>
+          </Routes>
         </div>
-        
-        
+      </div>
+      <footer className="footer">
+          <p>Aidan Hong 2024</p>
+        </footer>
     </>
   );
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,30 @@
 import headshot from "./Headshot.png";
+import { Link, Route, Routes } from "react-router-dom";
+import aboutimg from "./about-quick.png";
+import projectsimg from "./projectsimg.jpeg";
+import contactimg from "./Apple_Mail-512.webp";
 export default function Home(){
     return(
-    <div class="side">
-        <div class="text">
-            <h1>Aidan Hong</h1>
-            <h2>Computer Engineer+Fencer</h2>
+    <div class="contentwrap">
+        <div class="text-fade-in">
+            <h1>Hey there!</h1>
+            <h3>My name is Aidan Hong. I am an aspiring computer engineer.</h3>
+            <p></p>
+            <p>Using Technology to Innovate a Better World</p>
         </div>
-        <div class="image">
-            <img class="headshot" src={headshot} alt="headshot"/>
+        <div class="quicklinks">
+            <div class="quicksection">
+                <Link to="/about"><img class="image"src={aboutimg}></img></Link>
+                <p>About Me</p>
+            </div>    
+            <div class="quicksection"> 
+                <Link to="/projects"><img class="image"src={projectsimg}></img></Link>
+                <p>Projects</p>
+            </div>  
+            <div class="quicksection">
+                <Link to="/contact"><img src={contactimg}></img></Link>
+                <p>Contact Me!</p>
+            </div>      
         </div>
     </div>
     );
