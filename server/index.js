0f0614401af11811e31ec6d2cef8a5f9148a2476
GSWForever8 <aidanhong8@icloud.com>
2024-06-06 13:50:02 -0700

    Fixed backend code to account for new domain

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(express.static(buildPath));
 
 
 const corsOptions={
-  origin:'https://portfolio-aidan-hong-frontend.onrender.com',
+  origin:['https://portfolio-aidan-hong-frontend.onrender.com','https://aidanhong.com'],
   optionsSuccessStatus:200
 }
 app.use(cors(corsOptions))
